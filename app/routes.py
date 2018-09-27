0b252820d5adf2753998355a916cf6e475025edf
vagrant <vagrant@jhmaster.puppet>
2018-09-27 12:44:05 +0000

    Working app

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,3 @@
-# -*- coding: future_fstrings -*-
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import PostForm, DeleteForm, UpdateForm
@@ -22,15 +21,15 @@ def index():
     date = datetm.strftime("%Y-%m-%d")
     if form.validate_on_submit():
         post = post.replace("'", "\\'")
-	c.execute(f"insert into tbl_posts (name, posts, datetime) values('{name}','{post}','{date}')")
+	c.execute("insert into tbl_posts (name, posts, datetime) values({name},{post},{date})").format(name=name, post=post, date=date)
 	db.commit()
 	return redirect(url_for('forum'))
     return render_template('index.html', title='Home', form=form)
 
 @app.route('/forum')
 def forum():
-    c.execute(f"SELECT * FROM tbl_posts")
-    base = f"<html><head><title>QA FORUM</title></head><body><div>QA Forum: <a href=\"/index\">Home</a> <a href=\"/forum\">Forum</a> <a href=\"/delete\">Delete</a> <a href=\"/update\">Update</a></div>"
+    c.execute("SELECT * FROM tbl_posts")
+    base = "<html><head><title>QA FORUM</title></head><body><div>QA Forum: <a href=\"/index\">Home</a> <a href=\"/forum\">Forum</a> <a href=\"/delete\">Delete</a> <a href=\"/update\">Update</a></div>"
 
     s = base + "<h1>Welcome to QA Forum!</h1><h1>These are the posts!</h1><table style='border:1px solid black'>"  
     for row in c:  
@@ -46,7 +45,7 @@ def delete():
     form = DeleteForm()
     id = form.id.data
     if form.validate_on_submit():
-	c.execute(f"DELETE FROM tbl_posts WHERE user_id={id}")
+	c.execute("DELETE FROM tbl_posts WHERE user_id={id}".format(id=id))
 	db.commit()
 	flash("Successfully deleted Post")
         return redirect(url_for('forum'))
@@ -61,7 +60,7 @@ def update():
     post = form.post.data
     if form.validate_on_submit():
 	post = post.replace("'", "\\'")
-	c.execute(f"UPDATE tbl_posts SET posts='{post}', datetime='{date}' WHERE user_id='{id}'")
+	c.execute("UPDATE tbl_posts SET posts='{post}', datetime='{date}' WHERE user_id='{id}'".format(post=post, date=date, id=id))
 	return redirect(url_for('forum'))
     return render_template('update.html', title='Update', form=form)
 
